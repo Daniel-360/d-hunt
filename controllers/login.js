fa9b0ac3ae011371cf8d37def56318e593002e4c
daniel iyadi <danieliyadi7@gmail.com>
2023-06-27 16:31:41 +0100

    makiing few changes in controller

--- controllers/login.js
+++ controllers/login.js
@@ -2,26 +2,26 @@
 let userData = require('../model/userData')
 let users = require('../model/userSchema')
 
-exports.demoLogin = (req, res)=>{
+exports.demoLogin = (req, res) => {
     req.session.isLogin = 0; // asignin value 0 to session if not login
     // req.session.error = null
-    if (req.session.isLogin != 0 ) {
+    if (req.session.isLogin != 0) {
         // res.send('user already logged in')
         req.session.error = 'user is already login'
         res.redirect('/login/login')
     } else {
         // req.session.isLogin = 1;
-        let user = userData.find((user)=> user.email === req.body.email)
-        
+        let user = userData.find((user) => user.email === req.body.email)
+
         if (!user) {
             req.session.error = 'not a user pls sign up below'
             res.redirect('/login')
             // req.session.isLogin = 0
-        } else{
+        } else {
             if (user.password !== req.body.password) {
                 req.session.error = 'incorrect password'
                 res.redirect('/login/login')
-            } else{
+            } else {
                 req.session.isLogin = 1
                 req.session.error = null
                 req.session.username = user.username
@@ -36,33 +36,33 @@ exports.demoLogin = (req, res)=>{
 }
 
 
-exports.checkLogin = (req, res)=>{
+exports.checkLogin = (req, res) => {
     req.session.isLogin = 0
 
-    users.findOne({email: req.body.email})
-    .then((users)=>{
-        if (!users) {
-            res.send('not a user pls sign in')
-        } else{
+    users.findOne({ email: req.body.email })
+        .then((users) => {
+            if (!users) {
+                res.send('not a user pls sign up')
+            } else {
 
-            if (users.password !== req.body.password) {
-                req.session.error = 'incorrect password'
-                res.redirect('/login/login')
+                if (users.password !== req.body.password) {
+                    req.session.error = 'incorrect password'
+                    res.redirect('/login/login')
+                }
+                req.session.isLogin = 1
+                req.session.email = req.body.email
+                req.session.role = users.role
+                req.session.tel = users.tel
+                console.log(users);
+                // res.redirect('/login/home')
+                res.send('/login/home')
+                // res.send('backend working')
             }
-            req.session.isLogin = 1
-            req.session.email = req.body.email
-            req.session.role = users.role
-            req.session.tel = users.tel
-            console.log(users);
-            // res.redirect('/login/home')
-            res.send('/login/home')
-            // res.send('backend working')
-        }
 
-    })
+        })
 }
 
-exports.signUp = (req,res)=>{
+exports.signUp = (req, res) => {
     req.session.isLogin = 0
     // const {email, tel, password, confirmPass} = req.body
     // check if passwords are the same
@@ -70,32 +70,33 @@ exports.signUp = (req,res)=>{
     if (req.body.password == req.body.confirmPass) {
         console.log(req.body.password, req.body.confirmPass);
 
-        users.exists({ email: req.body.email})
-        .then((exists) => {
-          if (exists) {
-            res.send('email already exist pls sign in');
-          }
-    
-          // Create a new user
-          let newUser = new users({
-            email: req.body.email,
-            tel: req.body.tel,
-            password: req.body.password,
-            role: 'user'
-        })
-          // Save the user to the database
-          return newUser.save();
-        })
-        .then(() => {
-          res.redirect('/login/home');
-        })
-        .catch((error) => {
-          res.status(500).json({ error: 'Sign up failed' });
-        }); 
-    } else{
+        users.exists({ email: req.body.email })
+            .then((exists) => {
+                if (exists) {
+                    res.send('email already exist pls sign in');
+                }
+                // Create a new user
+                let newUser = new users({
+                    email: req.body.email,
+                    tel: req.body.tel,
+                    password: req.body.password,
+                    role: 'user'
+                })
+                // Save the user to the database
+                return newUser.save();
+            })
+            .then(() => {
+                res.redirect('/login/home');
+            })
+            .catch((error) => {
+                res.status(500).json({ error: 'Sign up failed' });
+            });
+    } else {
         req.session.error = 'password not the same'
-        res.redirect('/login/signUp')
+        res.send('/login/signUp')
     }
 
 }
+
+
 
